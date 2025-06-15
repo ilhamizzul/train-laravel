4d9396e69056110c831e2b946dc49a78ca33cda0
ilhamizzul <ilhamizzul@gmail.com>
2025-06-15 21:31:39 +0700

    adding model anstatus column for blogs

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -11,7 +12,8 @@ class BlogController extends Controller
      */
     public function index()
     {
-        //
+        $blogs = Blog::all();
+        return dd($blogs);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SingleActionController;
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
@@ -19,9 +20,7 @@ Route::get('/contact', function () {
 })->name('contact');
 
 Route::group(['prefix' => 'blog', 'as' => 'blog.'], function() {
-    Route::get('/', function() {
-        return "Blog Page";
-    });
+    Route::get('/', [BlogController::class, 'index']);
 
     Route::get('/create', [BlogController::class, 'create'])->name('create');
 
