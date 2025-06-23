d861c2d790dcd8e41a60806d2a0e27e75eaf8754
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2025-06-23 16:26:25 +0700

    insert new data using query builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     function index() {
+
+        DB::table('users')->insert([
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'password' => 'user123',
+            'email_verified_at' => now(),
+            'created_at' => now()
+        ]);
+
         return view('welcome');
         // return 'this is content from home controller';
     }
